7ab84f636fd1aceab9424c616bc04535a586b7ea
seyfettiny <334321@ogr.ktu.edu.tr>
2022-06-09 21:54:39 +0300

    For, While loops.

---
+++ src/main/kotlin/loop/2 While.kt
@@ -0,0 +1,19 @@
+package loop
+
+fun main() {
+    //  While loops include both for and if functionality, so using while may gain readability
+    //  and a little performance.
+
+    var number = 0
+
+    while (number < 5) {
+        print("${number++}")
+    }
+
+    //  Same as above
+    //    for (value in 0..5) {
+    //        if (value < 5)
+    //            print("${number++}")
+    //    }
+
+}
